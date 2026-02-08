350c47bcf5497e37be49c20391a23274f8a1130d
Hit Tusharbhai Narola <kanonarola8@gmail.com>
2026-02-08 12:30:10 +0000

    adding the middleware

--- middleware/loger.js
+++ middleware/loger.js
@@ -1,5 +1,5 @@
 const fs = require("node:fs");
-
+//adding the which method is used in the log.txt file
 exports.loger = function (req, res, next) {
   const log = `\n[${Date.now()}] ${req.method} ${req.path}`;
   fs.appendFileSync("log.txt", log, "utf-8");
